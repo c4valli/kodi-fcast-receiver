9c407cd14030dfbc4ca97f985c739f5c01c1d646
Samuele Guerrini <sguerrini97@gmail.com>
2023-12-30 11:17:59 +0100

    updated FCast packets

--- resources/lib/fcast_plugin/FCastPackets.py
+++ resources/lib/fcast_plugin/FCastPackets.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from enum import Enum
 
 class PlayBackState(int, Enum):
@@ -6,25 +7,38 @@ class PlayBackState(int, Enum):
     PAUSED = 2
 
 class PlayMessage:
-    def __init__(self, container: str, time: int = None, url: str = None, content: str = None) -> None:
+    def __init__(self, container: str, url: str = None, time: float = None, content: str = None, speed: float = 1.0) -> None:
         self.container = container
         self.url = url
         self.content = content
         self.time = time
+        self.speed = speed
 
 class SeekMessage:
-    def __init__(self, time: int) -> None:
+    def __init__(self, time: float) -> None:
         self.time = time
 
 class PlayBackUpdateMessage:
-    def __init__(self, time: int, state: PlayBackState) -> None:
+    def __init__(self, time: float, duration: float, speed: float, state: PlayBackState, generationTime: int = None) -> None:
         self.time = time
+        self.duration = duration
+        self.speed = speed
         self.state = state
+        self.generationTime = generationTime if generationTime else datetime.now(timezone.utc).timestamp() * 1000
 
 class VolumeUpdateMessage:
-    def __init__(self, volume: float) -> None:
+    def __init__(self, volume: float, generationTime: float = None) -> None:
         self.volume = volume
+        self.generationTime = generationTime if generationTime else datetime.now(timezone.utc).timestamp() * 1000
 
 class SetVolumeMessage:
     def __init__(self, volume: float) -> None:
         self.volume = volume
+
+class SetSpeedMessage:
+    def __init__(self, speed: float) -> None:
+        self.speed = speed
+
+class PlaybackErrorMessage:
+    def __init__(self, message: str) -> None:
+        self.message = message
